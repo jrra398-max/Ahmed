274628b6c6a393378a2de953df7a54db826a9c8a
jrra398-max <jrra398@gmail.com>
2026-09-26 16:18:52 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
+import App from "./App";
 
-function App() {
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "100vh",
-        background: "#050505",
-        color: "white",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontFamily: "Arial, sans-serif",
-        fontSize: "40px",
-        letterSpacing: "8px",
-      }}
-    >
-      NOIR
-    </div>
-  );
-}
+const root = document.getElementById("root");
 
-createRoot(document.getElementById("root")).render(
-  <App />
+createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
